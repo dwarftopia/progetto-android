199829b63625804243310ea7d96e163711936045
IN5C11 <vansang.hocorp@gmail.com>
2021-02-01 10:35:35 +0100

    commit

--- app/src/main/java/com/example/progetto_android/GameScreen.java
+++ app/src/main/java/com/example/progetto_android/GameScreen.java
@@ -4,9 +4,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AlertDialog;
